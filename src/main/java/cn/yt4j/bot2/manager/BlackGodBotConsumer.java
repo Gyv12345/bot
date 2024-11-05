38d296963d24e20a775ad3deab446162f86874d0
shichenyang <gyv12345@163.com>
2024-11-05 15:37:49 +0800

    feat: 群主回复D删除用户信息

--- src/main/java/cn/yt4j/bot2/manager/BlackGodBotConsumer.java
+++ src/main/java/cn/yt4j/bot2/manager/BlackGodBotConsumer.java
@@ -4,12 +4,19 @@ import cn.hutool.core.util.ObjectUtil;
 import cn.yt4j.bot2.config.TelegramProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.ForwardMessage;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChatMember;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.RestrictChatMember;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.objects.ChatPermissions;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.chat.Chat;
+import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMember;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
@@ -74,10 +81,49 @@ public class BlackGodBotConsumer implements LongPollingSingleThreadUpdateConsume
                         Chat chat = update.getMessage().getChat();
                         // 新的一个群组 或者 频道，将其信息保存到数据库
                         log.info("New chat: {}, chatId: {}", chat.getTitle(), chatId);
-
-
                     }
                 }
+            }
+            // 检查是否为回复消息，如果是，则判断是否为禁止发送消息
+            checkAndRestrict(update.getMessage());
+        }
+    }
+
+    @Async
+    public void checkAndRestrict(Message message) {
+        // 确保消息是回复消息
+        if (message.isReply() && message.getReplyToMessage() != null) {
+            Long chatId = message.getChatId();
+            User replyUser = message.getFrom(); // 获取回复人
+            User originalUser = message.getReplyToMessage().getFrom(); // 获取被回复的原始消息发送者
+            // 禁止发送消息
+            String text = message.getText();
+            try {
+                // 获取回复人在群组中的身份
+                GetChatMember getChatMember = new GetChatMember(chatId.toString(),replyUser.getId());
+                ChatMember chatMember = telegramClient.execute(getChatMember);
+                // GroupAnonymousBot
+                // 判断回复人是否为群主，如果是群组的身份发送信息，username应该是GroupAnonymousBot，那么最好也把创建者加上
+                if (("GroupAnonymousBot".equals(chatMember.getUser().getUserName()) && "D".equals(text))
+                        || ("creator".equals(chatMember.getStatus()) && "D".equals(text))) {
+                    // 删除被回复的消息
+                    DeleteMessage deleteMessage = new DeleteMessage(chatId.toString(), message.getReplyToMessage().getMessageId());
+                    telegramClient.execute(deleteMessage);
+
+                    // 禁止发送消息
+                    ChatPermissions permissions = new ChatPermissions();
+                    permissions.setCanSendMessages(false);
+
+                    // 限制原消息发送者的权限
+                    RestrictChatMember restrictChatMember = new RestrictChatMember(chatId.toString(),originalUser.getId(),permissions);
+                    restrictChatMember.setChatId(chatId.toString());
+                    restrictChatMember.setUserId(originalUser.getId());
+                    restrictChatMember.setUntilDate((int) (System.currentTimeMillis() / 1000) + 3600); // 设置限制时长（1小时）
+
+                    telegramClient.execute(restrictChatMember);
+                }
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
         }
     }
